c74e9f2d62f824a0808bbb4bbcb31ace6d625ece
huanghao <huanghao@yy.com>
2020-04-05 19:24:08 +0800

    fix bug: close chan to fan out stop event

--- greeter_server/main.go
+++ greeter_server/main.go
@@ -50,15 +50,44 @@ type greeter struct {
 	router        rt.RouterClient
 }
 
-// SayHello implements helloworld.GreeterServer
-func (s *greeter) SayHello(ctx context.Context, req *pb.HelloRequest) (res *pb.HelloReply, err error) {
-
-	// first we do a server-to-server RPC to know the trace
-	if route, err := s.router.GetRoute(ctx, &rt.GetRouteReq{}); err == nil {
+func (s *greeter) getRoute(ctx context.Context) {
+	if route, err := s.router.GetRoute(ctx, &rt.GetRouteReq{}); err != nil {
+		log.Printf(" --- route --- error: %v", err)
+	} else if len(route.Routes) == 0 {
+		log.Printf(" --- route --- no routes")
+	} else {
 		for _, x := range route.Routes {
 			log.Printf(" --- route --- %q -> %q", x.ClientIdentity, x.ServerAddress)
 		}
 	}
+}
+
+func (s *greeter) addRoute(ctx context.Context, id, addr string) {
+	if _, err := s.router.AddRoute(ctx, &rt.AddRouteReq{
+		Route: &rt.Route{
+			ClientIdentity: id,
+			ServerAddress:  addr,
+		},
+	}); err != nil {
+		log.Printf(" --- add route --- error: %v", err)
+	}
+}
+
+func (s *greeter) delRoute(ctx context.Context, id string) {
+	if _, err := s.router.DelRoute(ctx, &rt.DelRouteReq{
+		Route: &rt.Route{
+			ClientIdentity: id,
+		},
+	}); err != nil {
+		log.Printf(" --- del route --- error: %v", err)
+	}
+}
+
+// SayHello implements helloworld.GreeterServer
+func (s *greeter) SayHello(ctx context.Context, req *pb.HelloRequest) (res *pb.HelloReply, err error) {
+
+	// first we do a server-to-server RPC to know the trace
+	s.getRoute(ctx)
 
 	md, _ := metadata.FromIncomingContext(ctx)
 	forwarded := md["x-forwarded-for"]
@@ -76,15 +105,10 @@ func (s *greeter) SubscribeNotice(req *pb.SubscribeRequest, srv pb.Greeter_Subsc
 	md, _ := metadata.FromIncomingContext(srv.Context())
 	forwarded := md["x-forwarded-for"]
 	log.Printf("subscribed by %q from %v", req.Identity, forwarded)
-	s.router.AddRoute(context.Background(), &rt.AddRouteReq{
-		Route: &rt.Route{
-			ClientIdentity: req.Identity,
-			ServerAddress:  s.service.Address(),
-		},
-	})
+	s.addRoute(context.TODO(), req.Identity, s.service.Address())
 	defer func() {
 		log.Printf("un-subscribed %q on %v", req.Identity, err)
-		s.router.DelRoute(context.Background(), &rt.DelRouteReq{Route: &rt.Route{ClientIdentity: req.Identity}})
+		s.delRoute(context.TODO(), req.Identity)
 	}()
 	for i := 0; ; i++ {
 		msg := fmt.Sprintf("%q notice %q: %d", s.service.Address(), req.Identity, i)
@@ -102,7 +126,7 @@ func (s *greeter) SubscribeNotice(req *pb.SubscribeRequest, srv pb.Greeter_Subsc
 func newGreeter(s service.Service) (g *greeter) {
 	g = &greeter{
 		service: s,
-		stop:    make(chan struct{}, 1),
+		stop:    make(chan struct{}),
 	}
 
 	conn, err := s.Registry().Dial(context.Background(), "com.github.aclisp.grpcpadentic.router", grpc.WithInsecure(), grpc.WithBlock(), grpc.WithTimeout(5*time.Second))
@@ -114,7 +138,7 @@ func newGreeter(s service.Service) (g *greeter) {
 }
 
 func (s *greeter) Stop() {
-	s.stop <- struct{}{}
+	close(s.stop)
 }
 
 func main() {
